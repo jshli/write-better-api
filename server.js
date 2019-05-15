87f451fd687811ccd7e79aec39300ae7c39c1c06
jshli <joshua.li@mu-aa.com>
2019-05-15 21:50:48 +1000

    refactoring with auth0 library

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-require('dotenv').config({path: __dirname + '/.env'})
+require('dotenv').config()
 const app = require("express")();
 const stripe = require("stripe")(process.env.STRIPE_KEY)
 var contentful = require('contentful');
 const axios = require("axios");
 var bodyParser = require('body-parser')
+var ManagementClient = require('auth0').ManagementClient;
 
 var client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 })
 
-
+var auth0 = new ManagementClient({
+  domain: `athena.au.auth0.com`,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  scope: "read:users update:users update:users_app_metadata read:users_app_metadata",
+  audience: "https://athena.au.auth0.com/api/v2/",
+  tokenProvider: {
+    enableCache: true,
+    cacheTTLInSeconds: 10
+  }
+})
 
 app.use(bodyParser.json());
 
@@ -28,95 +39,42 @@ app.get("/", (req, res) => {
 app.post("/charge", async (req, res) => {
   var request = req.body;
   var totalAmount = 0;
-  // get the prices of all the books first
-  //refactor this so there's no need to filter down the track
-  client.getEntries({
-    content_type:'book',
-  })
-  .then(function (entries) {
+  try {
+    const entries = await client.getEntries({
+      content_type:'book',
+    })
     totalAmount = entries.items
       .filter(item => request.items.includes(item.fields.title))
       .map(item => Math.round(Number(item.fields.price)*100))
       .reduce((acc, val) => acc + val);
-    stripe.charges.create({
+  } catch(e) {
+    return res.status(500).json({error: e})
+  }
+  try {
+    const response = await stripe.charges.create({
       amount: totalAmount,
       currency: "aud",
       description: "An example charge",
       source: request.token
     })
-    .then(response => {
-      res.send(response)}
-    )
-    //this catches declined transactions
-    .catch(err => {
-      return res.status(402).json({error: err.message})
-    })    
-
-  })
-  .catch(function(err) {
-    //this is catching contentful errors
-    //what happens IF this fails?
-    return res.status(500).json({error: err})
-  })
+    res.send(response)
+  } catch(e) {
+    return res.status(402).json({error: e.message})
+  }
 });
-
 
 app.post("/update", async (req, res) => {
   const response = req.body
-  console.log("body", req.body)
-  var token ="";
-  const url = 'https://athena.au.auth0.com/oauth/token';
-  const options = {
-    headers: { 
-      'content-type': 'application/json' ,
+  const params = {id: response.userId};
+  var metadata = {
+    books: response.bookIds
+  }
+  auth0.updateAppMetadata(params, metadata, function(err, user) {
+    if (err) {
+      return res.status(500).json({error: err})
     }
-  }
-  const data = { 
-    "client_id":process.env.AUTH0_CLIENT_ID,
-    "client_secret":process.env.AUTH0_CLIENT_SECRET,
-    "audience":"https://athena.au.auth0.com/api/v2/",
-    "grant_type":"client_credentials" 
-  }
-
-  try {
-    const result = await axios.post(url, data,options)
-    token = await result.data["access_token"]
-  } 
-  catch(e) {
-    console.log("error in the post request", e.message)
-  }
-  try {
-    const headers = { authorization: `Bearer ${token}`, 'content-type': 'application/json' }
-    const result = await axios.patch(`https://athena.au.auth0.com/api/v2/users/${response.userId}`, {
-      app_metadata: {
-        books: response.bookIds
-      }
-    }, headers)
-    console.log("patch request successful", result)
-  } 
-  catch(e) {
-    console.log("error in the patch request", e)
-  }
-  
+    res.send(user)
+  })
 })
 
-
 app.listen((process.env.PORT || 9000), () => console.log("Listening on port 9000"))
-
-// axios.post(url, data, options)
-  // .then(result => {
-  //   token = result.data["access_token"]
-  //   var patchOptions = {
-  //     url: `https://athena.au.auth0.com/api/v2/users/${response.userId}`,
-  //     headers: { Authorization: `Bearer ${token}`, 'content-type': 'application/json' },
-  //   }
-  //   return axios.patch(patchOptions.url, {
-  //     app_metadata: {
-  //       books: response.bookIds
-  //     }
-  //   }, patchOptions.headers)
-  //   .then(result => res.send(result))
-  //   .catch(err => res.status(500).json(err))
-  // })
-  // .then(result => res.send(result))
-  // .catch(err => console.log("there's an error", err))
